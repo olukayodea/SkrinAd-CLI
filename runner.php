970106e37e6b6bde360cf5b5958980542b129968
Olukayode <olukayode.adebiyi@hotmail.co.uk>
2022-12-19 12:56:37 -0500

    Update runner.php

--- runner.php
+++ runner.php
@@ -33,12 +33,11 @@
 					foreach( $bulkData as $data ) {
 						$avail_date = explode("_", $data['avail_date']);
 						if (array_search($today, $avail_date) !== false ) {	
-							print_r($data);
 							$total = $this->total($data['ref']);
 							$used_imp = $data['used_imp'];
 
 							// if ($used_imp < $total) {
-								if ((int) $this->dailCap($data['ref']) < ((int) $data['daily_cap']*0.75)) {
+								// if ((int) $this->dailCap($data['ref']) < ((int) $data['daily_cap']*0.75)) {
 									$r = rand(10,250);
 									for ($l = 0; $l < $r; $l++) {
 										$randomUser = $this->lists("users", false, 1, "RAND", "ASC", "`ageRange` = '".$this->ageRange[array_rand($this->ageRange)]."' AND `gender` = '".$this->gender[array_rand($this->gender)]."'", "getRow");
@@ -77,7 +76,7 @@
 										}
 									}
 
-								}
+								// }
 							// }
 						}
 					}
