1c5c44d798e6783210fc9480b56ee711a08b6a45
Olukayode <olukayode.adebiyi@hotmail.co.uk>
2022-12-19 12:38:22 -0500

    Update main.php

--- main.php
+++ main.php
@@ -8,7 +8,7 @@
 	$db = $database->connect();
 
     $count = $database->lists("spoof_log", false, false, "RAND", "", false, "count");
-    $random = rand(100,150);
+    $random = rand(50,100);
     if ($count > $random) {
         $limit = $random;
     } else {
