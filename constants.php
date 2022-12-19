5e163d74ecefaca023bc3c256fef169c7afe6be1
Olukayode <olukayode.adebiyi@hotmail.co.uk>
2022-12-19 12:50:23 -0500

    Update constants.php

--- constants.php
+++ constants.php
@@ -1,21 +1,9 @@
 <?php
 include_once("cred.php");
 
-    define("URL", $URL);
-    define("servername",  $servername);
-    define("dbusername",  $dbusername);
-    define("dbpassword",  $dbpassword);
-    define("dbname",  $dbname);
-
-echo $URL;
-echo "<br>";
-echo $servername;
-echo "<br>";
-echo $dbusername;
-echo "<br>";
-echo $dbpassword;
-echo "<br>";
-echo $dbname;
-echo "<br>";
-
+define("URL", $URL);
+define("servername",  $servername);
+define("dbusername",  $dbusername);
+define("dbpassword",  $dbpassword);
+define("dbname",  $dbname);
 ?>
